e5f71d26ce2e7e79a64cbcccb24bb312e62fd287
David <qiankunli@yahoo.com>
2019-02-23 21:09:31 +0800

    music play

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,5 +1,8 @@
 // components/classic/music/index.js
 import Behavior from '../classic-beh'
+
+const audioManager = wx.getBackgroundAudioManager()
+
 Component({
   behaviors: [Behavior],
   
@@ -7,7 +10,26 @@ Component({
     musicSrc: String
   },
 
-  methods: {
+  data: {
+    playing: false
+  },
 
+  methods: {
+    onControl() {
+      const playing = this.data.playing
+
+      audioManager.src = this.properties.musicSrc
+      audioManager.title = 'Music'
+
+      if (playing) {
+        audioManager.pause()
+      } else {
+        audioManager.play()
+      }
+
+      this.setData({
+        playing: !playing
+      })
+    }
   }
 })
