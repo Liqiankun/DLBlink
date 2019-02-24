e1d24b25d1d3942419464543ee06aaa97a0ff6c2
David <qiankunli@yahoo.com>
2019-02-24 11:03:30 +0800

    music play

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -7,19 +7,28 @@ Component({
   behaviors: [Behavior],
   
   properties: {
-    musicSrc: String
+    musicSrc: String,
+    title: String,
   },
 
   data: {
     playing: false
   },
 
+  lifetimes: {
+    attached() {
+      this._recoverStatus()
+    }
+  },
+
+
   methods: {
     onControl() {
       const playing = this.data.playing
+      const { musicSrc, title } = this.properties 
 
-      audioManager.src = this.properties.musicSrc
-      audioManager.title = 'Music'
+      audioManager.src = musicSrc
+      audioManager.title = title
 
       if (playing) {
         audioManager.pause()
@@ -30,6 +39,18 @@ Component({
       this.setData({
         playing: !playing
       })
+    },
+
+    _recoverStatus() {
+      if (audioManager.paused) {
+        this.setData({
+          playing: false
+        })
+      } else if (audioManager.src === this.properties.musicSrc) {
+        this.setData({
+          playing: true
+        })
+      }
     }
   }
 })
